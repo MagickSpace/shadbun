06fce12ff00ca43c17d7be59919e01947edbd814
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2024-02-05 23:03:15 +0000

    +tina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()]
+  site: 'https://shadbun.vercel.app',
+  integrations: [react(), mdx(), tailwind(), sitemap({
+    entryLimit: 10000
+  }), partytown()]
 });
