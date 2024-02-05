d83437499d43cf1b4de810e67fc11dbfd5f334a7
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2024-02-05 23:54:41 +0000

    +

---
+++ tina/config.tsx
@@ -0,0 +1,114 @@
+import { CATEGORIES } from '../src/data/categories.ts'
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: '1a8f4069-9160-4382-a2c2-42647099b101', // Get this from tina.io
+  token: 'db269ae55694e53b64cd0ec710f028f267fd5113', // Get this from tina.io
+  build: {
+		outputFolder: 'admin',
+		publicFolder: 'public'
+	},
+	media: {
+		tina: {
+			mediaRoot: '/src/assets/images',
+			publicFolder: ''
+		}
+	},
+	schema: {
+		collections: [
+			{
+				name: 'post',
+				label: 'Blog Post',
+				path: 'src/content/blog',
+				format: 'mdx',
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+          router: () => '/',
+        },
+				fields: [
+					{
+						type: 'image',
+						label: 'Cover Image',
+						required: true,
+						name: 'heroImage',
+						description: 'The image used for the cover of the post'
+					},
+
+					{
+						type: 'string',
+						required: true,
+						name: 'category',
+						label: 'Category',
+						description: 'Select an category for this post',
+						options: [...CATEGORIES]
+					},
+					{
+						type: 'string',
+						label: 'description',
+						required: true,
+						name: 'description',
+						description: 'A short description of the post'
+					},
+					{
+						type: 'datetime',
+						name: 'pubDate',
+						label: 'Publication Date',
+						required: true
+					},
+					{
+						name: 'draft',
+						label: 'Draft',
+						type: 'boolean',
+						description: 'If this is checked the post will not be published'
+					},
+					{
+						type: 'string',
+						name: 'tags',
+						required: true,
+						label: 'Tags',
+						description: 'Tags for this post',
+						list: true,
+						ui: {
+							component: 'tags'
+						}
+					},
+					{
+						type: 'string',
+						name: 'title',
+						label: 'Title',
+						isTitle: true,
+						required: true
+					},
+					{
+						type: 'rich-text',
+						label: 'Body',
+						name: 'SButton',
+						isBody: true,
+						templates: [
+							// Custom Components
+							{
+								label: 'SButton',
+								name: 'SButton',
+								fields: [
+									{
+										type: 'rich-text',
+										label: 'SButton',
+										name: 'children',
+										isBody: true
+									}
+								]
+							}
+						]
+					}
+				]
+			}
+		]
+	}
+})
