1356137cc8c8b2d076ac8e18731b64e835b186df
MagickSpace <146006257+MagickSpace@users.noreply.github.com>
2024-02-05 23:13:00 +0000

    +

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,24 @@ import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import yaml from "@rollup/plugin-yaml";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [yaml()]
+  },
   site: 'https://shadbun.vercel.app',
-  integrations: [react(), mdx(), tailwind(), sitemap({
+  integrations: [
+    react(), 
+    mdx(), 
+    tailwind({
+    applyBaseStyles: false,
+  }), 
+  sitemap({
     entryLimit: 10000
-  }), partytown()]
+  }), 
+  partytown()]
 });
--- tina/config.ts
+++ tina/config.ts
@@ -1,37 +1,29 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
-
-  // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
-  token: process.env.TINA_TOKEN,
-
+  clientId: '1a8f4069-9160-4382-a2c2-42647099b101', // Get this from tina.io
+  token: 'db269ae55694e53b64cd0ec710f028f267fd5113', // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "public",
   },
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "images",
       publicFolder: "public",
     },
   },
-  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
       {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
+        name: "blog",
+        label: "Blog",
+        path: "src/content/blog",
+        format: 'mdx',
         fields: [
           {
             type: "string",
@@ -41,16 +33,18 @@ export default defineConfig({
             required: true,
           },
           {
+            type: "datetime",
+            name: "posted",
+            label: "Date Posted",
+            required: true,
+          },
+          {
             type: "rich-text",
             name: "body",
             label: "Body",
             isBody: true,
           },
         ],
-        ui: {
-          // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
-        },
       },
     ],
   },
